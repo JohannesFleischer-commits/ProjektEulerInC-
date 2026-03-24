94c705f5bacbd6dcd7c4862fa92212fa4fd6c80d
Johannes <Johannes.flei@gmx.de>
2026-03-24 08:14:04 +0100

    Aufgabe14

---
+++ CollatzFolge.cpp
@@ -0,0 +1,23 @@
+/*
+Aufgabe 14:
+
+Für die Menge der positiven ganzen Zahlen ist 
+folgende iterative Vorschrift definiert:
+
+Wenn n gerade ist:
+n-> n/2
+	​
+Wenn n ungerade ist:
+
+n-> 3n+1
+Wendet man diese Regeln wiederholt an, entsteht eine Zahlenfolge 
+(auch Collatz-Folge genannt).
+
+Startet man mit der Zahl n = 13, ergibt sich folgende Sequenz:
+
+13-> 40-> 20-> 10-> 5-> 16-> 8-> 4-> 12-> 1
+
+Diese Folge enthält insgesamt 10 Zahlen 
+(inklusive Start- und Endwert)
+
+*/
