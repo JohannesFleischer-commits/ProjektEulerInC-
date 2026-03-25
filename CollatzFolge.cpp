101d55b1958ef38827689a3ed8cc7af55c06b999
Johannes <Johannes.flei@gmx.de>
2026-03-25 09:07:39 +0100

    Add C++ build task configuration and implement Collatz sequence calculation

--- CollatzFolge.cpp
+++ CollatzFolge.cpp
@@ -15,7 +15,7 @@ Wendet man diese Regeln wiederholt an, entsteht eine Zahlenfolge
 
 Startet man mit der Zahl n = 13, ergibt sich folgende Sequenz:
 
-13-> 40-> 20-> 10-> 5-> 16-> 8-> 4-> 12-> 1
+13-> 40-> 20-> 10-> 5-> 16-> 8-> 4-> 2-> 1
 
 Diese Folge enthält insgesamt 10 Zahlen 
 (inklusive Start- und Endwert)
@@ -27,28 +27,39 @@ die längste Collatz-Folge?
 
 */
 
-#include <cmath>
 #include <iostream>
 
 
 int main (){
 
+	int maxLaenge=0;
+	int besteZahl=0;
 
-	for(int i=1; i<100000; i ++){
+
+	for(int i=1; i<1000000; i ++){
 
 		long long zahl =i; 
 		int zaehler=1; 
-
+		
 		while(zahl!=1){
 
 			if(zahl%2==0){
 				zahl=zahl/2;
 
 			}else{
-				zahl=3+zahl+1;
-
+				zahl=3*zahl+1;
 			}
+			zaehler++;		
 		}
-		zaehler++;
+
+		if(zaehler > maxLaenge){
+			maxLaenge = zaehler;
+			besteZahl = i; 
+		}	
 	}
-}
+
+	std::cout <<"Startzahl mit der laengsten Kette ist:" << besteZahl << std::endl;
+	std::cout <<"Laenge der Kette ist:" << maxLaenge <<std::endl;
+
+	return 0;
+} 
