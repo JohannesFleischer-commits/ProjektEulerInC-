ba410395d9cc77cfe3f2bb771873d31b31ad53d9
Johannes <Johannes.flei@gmx.de>
2026-03-25 09:34:38 +0100

    Aufgabe19

---
+++ SonntageZaehlen.cpp
@@ -0,0 +1,25 @@
+/*
+Aufgabe 19:
+
+Dir stehen folgende Informationen zur Verfügung
+
+Der 1. Januar 1900 war ein Montag.
+Die Monate September, April, Juni und November haben jeweils 30 Tage.
+Alle übrigen Monate haben 31 Tage mit Ausnahme des Februars:
+Im Normalfall hat er 28 Tage.
+In Schaltjahren hat er 29 Tage.
+
+Ein Schaltjahr ist jedes Jahr, das durch 4 teilbar ist.
+Ausnahme: Jahre, die durch 100 teilbar sind, sind keine Schaltjahre,
+es sei denn, sie sind zusätzlich durch 400 teilbar.
+
+Fragestellung:
+
+Wie viele Sonntage fallen im 20. Jahrhundert 
+(vom 1. Januar 1901 bis zum 31. Dezember 2000) 
+auf den ersten Tag eines Monats?
+
+
+
+
+*/
